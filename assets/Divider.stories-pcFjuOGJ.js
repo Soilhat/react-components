d0dbc9cea0049f2b94d050227552fb770a02a176
Soilhat <Soilhat@users.noreply.github.com>
2025-11-20 12:03:59 +0000

    deploy: 0a41072d94fd3d6380d232a426e9d654646c2d4c

--- assets/Divider.stories-pcFjuOGJ.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{C as a}from"./Container-Bw0gaiBc.js";import{H as t}from"./Heading-DVWFy5v0.js";import"./iframe-foW8KUVn.js";import"./preload-helper-PPVm8Dsz.js";const i=({className:d=""})=>e.jsx("hr",{className:`${d} border-gray-200 dark:border-gray-700 my-4 dark:my-4`});i.__docgenInfo={description:"",methods:[],displayName:"Divider",props:{className:{defaultValue:{value:"''",computed:!1},required:!1}}};const p={title:"Layout/Divider",parameters:{layout:"fullscreen"}},r={render:()=>e.jsxs(a,{children:[e.jsx(t,{title:"Divider Example"}),e.jsx("div",{children:"Above the divider"}),e.jsx(i,{}),e.jsx("div",{children:"Below the divider"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <Container>
-      <Heading title="Divider Example" />
-      <div>Above the divider</div>
-      <Divider />
-      <div>Below the divider</div>
-    </Container>
-}`,...r.parameters?.docs?.source}}};const v=["DividerExample"];export{r as DividerExample,v as __namedExportsOrder,p as default};
