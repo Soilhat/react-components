3e7af1ca6be6427c61ef05f250589b20ad0bd177
Soilhat <Soilhat@users.noreply.github.com>
2025-12-21 22:32:28 +0000

    deploy: 122c65f115125eb0329d0da10b115c36ec46ec18

--- Forms/TagInput.d.ts
+++ Forms/TagInput.d.ts
@@ -2,8 +2,9 @@ interface TagInputProps {
     label?: string;
     placeholder?: string;
     tags: string[];
+    suggestions?: string[];
     onChange: (tags: string[]) => void;
     className?: string;
 }
-export declare const TagInput: ({ label, placeholder, tags, onChange, className }: TagInputProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TagInput: ({ label, placeholder, tags, suggestions, onChange, className }: TagInputProps) => import("react/jsx-runtime").JSX.Element;
 export {};
---
+++ assets/formatter-QJ4M4OGQ-DxYFPt3v.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-Bl6HA_Bb.js";import{b as t}from"./iframe-BUqkNVfX.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BfM-pleq.js";import"./index-BpuQPbb_.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
