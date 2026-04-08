a33769b86fbf4909ac33a2f0d37a4ad94f720142
Soilhat <Soilhat@users.noreply.github.com>
2026-04-08 07:32:44 +0000

    deploy: 9a20b8fc94127b506f45f1f9e2301c91df9f5801

---
+++ assets/formatter-EIJCOSYU-BycCdyyb.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-D_pp-pq4.js";import{b as t}from"./iframe-D60bBnKF.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DpxA_Lnm.js";import"./index-DO34N8T-.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
